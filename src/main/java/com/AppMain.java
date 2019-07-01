b720b0d708bb4dc7064703e55babec58710b257f
liyanzhao <liyz15@lenovo.com>
2019-07-01 17:23:31 +0800

    临时保存

--- src/main/java/com/AppMain.java
+++ src/main/java/com/AppMain.java
@@ -2,9 +2,11 @@ package com;
  
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableEurekaClient //Eureka Client
 public class AppMain {
     public static void main(String[] args) {
