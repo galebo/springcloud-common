9fca3c92fbff5a9ef05a21212ecf1939925a8c09
liyanzhao <liyz15@lenovo.com>
2019-07-02 17:07:12 +0800

    保存下

--- src/main/java/com/galebo/study/springcloud/ClientController.java
+++ src/main/java/com/galebo/study/springcloud/ClientController.java
@@ -1,11 +1,13 @@
 package com.galebo.study.springcloud;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class ClientController {
 	@Value("${word}")
 	private String word;
